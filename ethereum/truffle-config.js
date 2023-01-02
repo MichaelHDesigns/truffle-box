0af08a84cae7e277234307867888b5ca0dbff290
Devilking6105 <43361618+MichaelHDesigns@users.noreply.github.com>
2023-01-01 21:21:49 -0500

    Update truffle-config.js

--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -4,21 +4,21 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const MNEMONIC = process.env.MNEMONIC;
 const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
-const ALCHEMY_API_ID = process.env.ALCHEMY_API_ID;
+//const ALCHEMY_API_ID = process.env.ALCHEMY_API_ID;
 
 // ALCHEMY ENDPOINTS
-const MAINNET_ENDPOINT_ALCHEMY = `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
-const KOVAN_ENDPOINT_ALCHEMY = `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
-const RINKEBY_ENDPOINT_ALCHEMY = `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
-const ROPSTEN_ENDPOINT_ALCHEMY = `https://eth-ropsten.alchemyapi.io/v2//${ALCHEMY_API_ID}`;
-const GOERLI_ENDPOINT_ALCHEMY = `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
+//const MAINNET_ENDPOINT_ALCHEMY = `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
+//const KOVAN_ENDPOINT_ALCHEMY = `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
+//const RINKEBY_ENDPOINT_ALCHEMY = `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
+//const ROPSTEN_ENDPOINT_ALCHEMY = `https://eth-ropsten.alchemyapi.io/v2//${ALCHEMY_API_ID}`;
+//const GOERLI_ENDPOINT_ALCHEMY = `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_ID}`;
 
 // INFURA ENDPOINTS
-const MAINNET_ENDPOINT_INFURA = `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`;
-const KOVAN_ENDPOINT_INFURA = `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`;
-const RINKEBY_ENDPOINT_INFURA = `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`;
-const ROPSTEN_ENDPOINT_INFURA = `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`;
-const GOERLI_ENDPOINT_INFURA = `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`;
+const MAINNET_ENDPOINT_INFURA = `https://altcoinchain.infura.io/v3/${INFURA_PROJECT_ID}`;
+//const KOVAN_ENDPOINT_INFURA = `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`;
+//const RINKEBY_ENDPOINT_INFURA = `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`;
+//const ROPSTEN_ENDPOINT_INFURA = `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`;
+//const GOERLI_ENDPOINT_INFURA = `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`;
 
 module.exports = {
   // OPTIONAL
